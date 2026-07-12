1f8ca424c98a6aa3c07fe12872a2a89140144ba8
Tris Forster <tris@tfconsulting.com.au>
2026-07-13 09:35:43 +1000

    Convert to PWA

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'notecards-v1';
+const ASSETS = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'static/notecards.css',
+  'static/quavers.png',
+  'static/quavers.svg',
+  'js/abcjs-basic-min.js',
+  'js/instruments.js',
+  'js/notecards.js',
+  'https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css',
+  'https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
